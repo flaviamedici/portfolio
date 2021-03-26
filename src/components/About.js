d1bba88f0e2d3d88b6e410616a49aad2a5620155
flaviamedici <m.olivares8915@edmail.edcc.edu>
2021-03-26 11:15:26 -0700

    Small updates

--- src/components/About.js
+++ src/components/About.js
@@ -25,9 +25,9 @@ export default function About() {
 
     return (
         <main className="relative">
-            <img src={About_pic} alt="MyPic" className="absolute w-full" />
+            <img src={About_pic} alt="MyPic" className="absolute w-full opacity-70" />
             <div className="p-10 lg:pt-48 container mx-auto relative">
-                <section className="bg-gray-800 rounded-lg shadow-2xl lg:flex p-20">
+                <section className="bg-gray-600 rounded-lg shadow-2xl lg:flex p-20">
                     <img src={urlFor(author.authorImage).url()} className="rounded w-32 lg:w-64 lg:h-64 mr-8"/>
                     <div className="text-lg flex flex-col justify-center">
                         <h1 classname="cursive text-6xl text-white mb-4">
